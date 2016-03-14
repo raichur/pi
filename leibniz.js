9f8021e64a7b08dbddcaa068e208b90e6e977ce7
= <joshua.raichur@gmail.com>
2016-03-14 14:23:19 -0500

    Add: Leibniz

---
+++ leibniz.js
@@ -0,0 +1,11 @@
+num_iterations = 1000; // The higher the number of iterations, the more accurate the value of pi calculated
+
+function calculate_pi(pi, count) {
+  for(var i = 0; i <= num_iterations; i++) {
+    pi += (4/count) - (4/(count+2));
+    count += 4;
+  }
+  return pi;
+}
+
+calculate_pi(0,1);
